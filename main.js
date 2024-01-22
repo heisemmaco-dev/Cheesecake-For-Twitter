46fbcb63166dfc34074ac7859ba22de8385bf55c
heisemmaco-dev <heisemmaco@gmail.com>
2024-01-22 19:53:03 +0100

    done all the functionality, style and structuring

--- main.js
+++ main.js
@@ -60,6 +60,7 @@ form.addEventListener("submit", (e) => {
   let emailErrorMessage = form.querySelector(".emailError");
   let textAreaErrorMessage = form.querySelector(".textareaError");
 
+  // checking if the input is empty when the submit button is clicked
   if (nameInput.value.trim() === "") {
     nameInput.style.border = "1px solid red";
     nameErrorMessage.innerHTML = "Name cannot be empty";
@@ -69,11 +70,12 @@ form.addEventListener("submit", (e) => {
     nameErrorMessage.innerHTML = null;
   }
 
+  // checking if the email input is empty and valid when the submit button is clicked
   if (emailInput.value.trim() === "") {
     emailInput.style.border = "1px solid red";
     emailErrorMessage.innerHTML = "Email cannot be empty";
     emailErrorMessage.style.color = "red";
-  } else if (!validateEmail(emailInput)) {
+  } else if (!validateEmail(emailInput.value)) {
     emailInput.style.border = "1px solid red";
     emailErrorMessage.innerHTML = "Looks like this is not an email";
     emailErrorMessage.style.color = "red";
@@ -82,6 +84,7 @@ form.addEventListener("submit", (e) => {
     emailErrorMessage.innerHTML = null;
   }
 
+  // checking if the text is empty when the submit button is clicked
   if (textArea.value.trim() === "") {
     textArea.style.border = "1px solid red";
     textAreaErrorMessage.innerHTML = "Message cannot be empty";
@@ -90,13 +93,40 @@ form.addEventListener("submit", (e) => {
     textArea.style.border = "none";
     textAreaErrorMessage.innerHTML = null;
   }
+
+  let submitBtn = form.querySelector("button");
+  submitBtn.innerHTML = "Loading...";
+
+  setTimeout(() => {
+    submitBtn.innerHTML = "Contact us now";
+  }, 400);
+
+  let congratulationMessage = form.querySelector(".congratulationMessage");
+
+  // after validting form input, i want it to show a congratulation message so the user is notify that is input have been submitted
+  if (
+    nameInput.value.trim() !== "" &&
+    textArea.value.trim() !== "" &&
+    emailInput.value.trim() !== "" &&
+    validateEmail(emailInput.value)
+  ) {
+    nameInput.value = null;
+    emailInput.value = null;
+    textArea.value = null;
+
+    congratulationMessage.style.color = "green";
+    congratulationMessage.innerHTML = "Thank for contacting us";
+
+    setTimeout(() => {
+      congratulationMessage.innerHTML = " ";
+    }, 3000);
+  }
 });
 
+// the funtion to validate the form
 function validateEmail(emailInput) {
-  // Regular expression for a valid email pattern
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-  // Test the email against the regular expression
   return emailRegex.test(emailInput);
 }
 
@@ -111,7 +141,7 @@ function reveal() {
     let windowheight = window.innerHeight;
     let revealtop = reveals[i].getBoundingClientRect().top;
 
-    let revealpoint = 150;
+    let revealpoint = 140;
 
     if (revealtop < windowheight - revealpoint) {
       reveals[i].classList.add("active");
